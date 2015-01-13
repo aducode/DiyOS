b39e91408ae121baaa37b76fea342fbe2c3029a7
Issac <aducode@126.com>
2015-01-13 16:19:58 +0800

    fmt boot sec to fat12 format

--- boot.asm
+++ boot.asm
@@ -1,77 +1,59 @@
+;boot.asm
+;nasm -o boot.bin boot.asm
+;软盘引导扇区
 org 0x7c00
-mov ax,cs
-mov ds,ax
-mov es,ax
-call clean_screen
+jmp short BOOT_START	;Start to boot
+nop			;fat12开始的jmp段长度要求为3
+BS_OEMName	DB	'DIYCOM  '	;长度必须8字节
+BPB_BytsPerSec	DW	512		;每扇区字节数
+BPB_SecPerClus	DB	1		;每簇多少扇区
+BPB_RsvdSecCnt	DW	1		;Boot 记录占用多少扇区
+BPB_NumFATs	DB	2		;共有多少FAT表
+BPB_RootEntCnt	DW	224		;根目录文件数最大值
+BPB_TotSec16	DW	2880		;逻辑扇区总数
+BPB_Media	DB	0xF0		;媒体描述符
+BPB_FATSz16	Dw	9		;每FAT扇区数
+BPB_SecPerTrk	DW	18		;每磁道扇区数
+BPB_NumHeads	DW	2		;磁头数
+BPB_HiddSec	DD	0		;隐藏扇区数
+BPB_TotSec32	DD	0		;wTotalSectorCount为0时这个记录扇区数
+BS_DrvNum	DB	0		;中断13的驱动器号
+BS_Reserved1	DB	0		;未使用
+BS_BootSig	DB	0x29		;扩展引导标记
+BS_VolID	DB	0		;卷序列号
+BS_VolLab	DB	'DiyOS_V0.01'	;卷标，必须11字节
+BS_FileSysType	DB	'FAT12   '		;文件系统类型，必须8个字节
 
-;call change_mode
-
-call disp_str
-;hlt ;注释掉，因为如果hlt了，就不能响应ctrl+c退出
-;jmp $-1
-jmp $
-
-change_mode:
-	mov ah,0
-	;mov al, 0x00 ;16色字符模式40 X 25
-	mov al, 0x03 ;16色字符模式 80 X 25
-	;mov al, 0x12 ;VGA图形模式,640X480X4位彩色模式
-	;mov al, 0x13 ;VGA图形模式，320X200X8位彩色模式
-	;mov al, 0x6a ;扩展VGA图形模式，800X600X4位彩色模式
-	int 0x10
-	ret
-
+BOOT_START:
+	;实模式下
+	mov ax,cs
+	mov ds,ax
+	mov es,ax
+	;显示
+	call clean_screen
+	call disp_str
+	jmp $
 clean_screen:
 	mov ax,0x0600
-	mov bx,0x0000
-	mov cx,0x0000
-	mov dl, 79
+	mov bx, 0x0000
+	mov cx, 0x0000
 	mov dh, 24
+	mov dl, 79
 	int 0x10
 	ret
-	
-disp_str:
+
+disp_str:	
 	mov ax, BootMessage
 	mov bp, ax
-	mov cx,len
-	mov ax, 0x1301 ;ah:0x13 显示服务功能号：显示字符串  al:0x01 显示模式
-	;显示模式(AL)
-	;    0x00:字符串只包含字符码，显示之后不更新光标位置，属性值在BL中
-	;    0x01:字符串只包含字符码，显示之后更新光标位置，属性值在BL中
-	;    0x02:字符串包含字符码及属性值，显示之后不更新光标位置
-	;    0x03:字符串包含字符码及属性值，显示之后更新光标位置
-	
-	mov bh, 0x00 ;视频页
-	;黑底
-	;mov bl, 0x00 ;黑
-	;mov bl, 0x01 ;蓝
-	;mov bl, 0x02 ;绿
-	;mov bl, 0x03 ;淡蓝
-	;mov bl, 0x04 ;红
-	;mov bl, 0x05 ;紫
-	;mov bl, 0x06 ;黄
-	mov bl, 0x07 ;白
-	;mov bl, 0x08 ;灰
-	;mov bl, 0x09 ;蓝
-	;mov bl, 0x0a ;亮绿
-	;mov bl, 0x0b ;亮蓝
-	;mov bl, 0x0c ;亮红
-	;mov bl, 0x0d ;亮紫
-	;mov bl, 0x0e ;亮黄
-	;mov bl, 0x0f ;亮白
-	;蓝底
-	;mov bl, 0x10 ;黑字
-	;mov bl, 0x11 ;蓝字
-	;; ... ...
-	;绿底
-	;mov bl, 0x20 ;黑字
-	;
-	mov dh, 0x00 ;起始行
-	mov dl, 0x00 ;起始列
-	int 0x10 ;BIOS 显示服务中断
+	mov cx, len
+	mov ax, 0x1301
+	mov bh, 0x00
+	mov bl, 0x07
+	mov dx, 0x0000
+	int 0x10
 	ret
 BootMessage:
-	db "hello, OS world!\nI'm your father!!!!"
+	db "hello world!"
 len equ $-BootMessage
 times 510 - ($-$$) db 0
 dw 0xaa55
