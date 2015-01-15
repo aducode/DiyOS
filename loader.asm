218f376e6cb66e2a29482618ebca9b004d3b03e5
Issac <aducode@126.com>
2015-01-15 09:21:05 +0800

    test loader.asm int 0x10

--- loader.asm
+++ loader.asm
@@ -1,7 +1,11 @@
 org 0x0100
-	mov ax, 0xb800	;字符模式显存首地址
-	mov gs, ax	
-	mov ah, 0x0F
+	mov ah, 0x0E
 	mov al, 'L'
-	mov [gs:((80*0+39)*2)], ax	;屏幕第0行 第39列
+	mov bl, 0x0F
+	int 0x10
+	;mov ax, 0xb800	;字符模式显存首地址
+	;mov gs, ax	
+	;mov ah, 0x0F
+	;mov al, 'L'
+	;mov [gs:((80*0+39)*2)], ax	;屏幕第0行 第39列
 	jmp $
