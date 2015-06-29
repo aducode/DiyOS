1459627edbf628e8af7225a6c9eff0b2595d1c15
Issac <aducode@126.com>
2015-06-29 20:50:10 +0800

    add #define

--- kernel/include/sys/floppy.h
+++ kernel/include/sys/floppy.h
@@ -41,11 +41,16 @@ extern void floppy_ioctl(struct message *msg);
 
 //IO port
 //I/O address			Read or Write			Register
-//0x3f2				Write					DOR: Digital Output Register
-//0x3f4				Read					FDC Status: Floppy Disk Status Register
-//0x3f5				Read/Write				FDC Data: Floppy Disk Data Register
-//0x3f7				Read					DIR: Digital Input Register
-//					Write					DCR: Disk Control Register
+//0x3f2					Write					DOR: Digital Output Register
+//0x3f4					Read					FDC Status: Floppy Disk Status Register
+//0x3f5					Read/Write				FDC Data: Floppy Disk Data Register
+//0x3f7					Read					DIR: Digital Input Register
+//						Write					DCR: Disk Control Register
+
+#define		REG_DOR			0x3f2
+#define		FDC_STATUS		0x3f4
+#define		FDC_DATA		0x3f5
+
 //DOR 数字输出寄存器
 //DOR是一个8为寄存器，他控制驱动器马达的开启、驱动器选择、启动/复位FDC以及允许/禁止DMA请求
 //位			Name		Description
@@ -79,7 +84,8 @@ extern void floppy_ioctl(struct message *msg);
 //
 //	0	0	0	0	0	1	1	1			0x07
 //	0	0	0	0	0	0	US1	US2
-//结果 刺头移动到track0
+//结果 磁头移动到track0
+#define		CMD_FD_RECALIBRATE				0x07
 //(2)磁头寻道命令（FD_SEEK）
 //把磁头定位到指定位置，在读写前执行
 //FD_SEEK cmd:
@@ -89,6 +95,7 @@ extern void floppy_ioctl(struct message *msg);
 //	0	0	0	0	0	HD	US1	US2			磁头(HD)号驱动器号
 //	C										磁道号
 //结果 把磁头定位到指定位置，在读写前执行
+#define		CMD_FD_SEEK						0x0F
 
 //(3)读扇区数据命令（FD_READ）
 //FD_READ cmd:
@@ -139,6 +146,7 @@ extern void floppy_ioctl(struct message *msg);
 //	2		ST2_SNS				检索条件不满足（Scan Not Satisfied）
 //	1		ST2_BC				磁道坏（Bad Cylinder）
 //	0		ST2_MAM				未找到扇区地址标志ID（Miss Address Mask）
+#define		CMD_FD_READ			0xE6	
 
 //(4)写扇区数据命令（FD_WRITE）
 //FD_WRITE cmd:
@@ -157,6 +165,7 @@ extern void floppy_ioctl(struct message *msg);
 //	ST0										状态字节0
 //	ST1										状态字节1
 //	ST2										状态字节2
+#define		CMD_FD_WRITE					0xC5
 
 //(5) 检测中断状态命令（FD_SENSEI）
 //FD_SENSEI cmd:
@@ -166,6 +175,7 @@ extern void floppy_ioctl(struct message *msg);
 //结果
 //	ST0										状态字节0
 //	C										磁头所在磁道号
+#define		CMD_FD_SENSEI					0x08
 
 //(6)设定驱动器参数命令（FD_SPECIFY）
 //FD_SPECIFY cmd:
@@ -175,5 +185,12 @@ extern void floppy_ioctl(struct message *msg);
 //	|	SRT		|		HUT		|			马达速度(2ms) 磁头卸载时间(32ms)
 //	|		HLT				|	ND			磁头加载时间(4ms) 非DMA模式
 //结果 设置控制器
-					
+#define		CMD_FD_SPECIFY					0x03
+
+//DIR:数字输入寄存器
+//DIR寄存器只有D7位有效，用于表示软盘更换状态，其余用于硬盘控制器。
+
+// DCR：磁盘控制寄存器
+//DCR仅是用户D0与D1位，用于表示数据传输率。
+//00-500kpbs, 01-300kpbs, 10-250kpbs					
 #endif
