4f2f09b97597ff7d12d1765ce503d7b3227e525f
Issac <aducode@126.com>
2015-07-24 20:08:47 +0800

    fix

--- kernel/include/exit.h
+++ kernel/include/exit.h
@@ -1,4 +1,11 @@
 #ifndef _DIYOS_EXIT_H
 #define _DIYOS_EXIT_H
+/**
+ * @function exit
+ * @brief Terminate the current process.
+ * 
+ * @param status The value returned to the parent.
+ *
+ */
 extern void exit(int status);
 #endif
--- kernel/include/wait.h
+++ kernel/include/wait.h
@@ -1,4 +1,12 @@
 #ifndef _DIYOS_WAIT_H
 #define _DIYOS_WAIT_H
+/**
+ * @function wait
+ * @brief Wait for the child process to terminiate.
+ * 
+ * @param status The value returned from the child.
+ *
+ * @return PID of the terminated child.
+ */
 extern int wait(int * status);
 #endif
