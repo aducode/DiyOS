15d79c64a3ac117a10aa2925f77a19536a3dedb5
Issac <aducode@126.com>
2015-07-25 16:27:18 +0800

    fix add flag  field in struct entry;before I use key field as flag ,but key's type is void * ,it can not be negative

--- kernel/common/map.c
+++ kernel/common/map.c
@@ -8,89 +8,93 @@ static struct entry * find(struct map *map, void *key);
 struct entry * find(struct map * map, void *key){
 	int hashcode = map->cal_hash_code(key) & (map->capacity - 1);
 	int first_hashcode = -1;
-	while(1){
-		if((map->entries+hashcode)->key == 0){
-			//key==0表示empty slot
+	while (1){
+		if (first_hashcode >0 && (map->entries + hashcode)->flag == UNUSE){
+			//flag==UNUSE表示未使用
 			break;
-		} 
-		if((map->entries + hashcode)->key!=-1 && map->cmp((map->entries + hashcode)->key, key) == 0)
+		}
+		if ((map->entries + hashcode)->flag == USING && map->cmp((map->entries + hashcode)->key, key) == 0)
 		{
-			//key == -1 说明处于冲突链中间
+			//flag == USING 说明正在被使用
 			//找到
 			return map->entries + hashcode;
 		}
 		//hash冲突，找下一个
-		if(first_hashcode == -1){
+		if (first_hashcode == -1){
 			first_hashcode = hashcode;
-		} else if (first_hashcode == hashcode){
+		}
+		else if (first_hashcode == hashcode){
 			//第二次first_hashcode 与 hashcode 相等时，说明已经全部遍历完了
 			break;
 		}
 		hashcode++;
-		if(hashcode>=capacity){
+		if (hashcode >= map->capacity){
 			hashcode = 0;
 		}
 	}
 	return 0;
 }
 /**
- * @function get
- * @brief 根据key获取value
- * @param map
- * @param key
- * @return  value
- */
-void * get(sturct map * map, void * key)
+* @function get
+* @brief 根据key获取value
+* @param map
+* @param key
+* @return  value
+*/
+void * get(struct map * map, void * key)
 {
 	struct entry * entry = find(map, key);
-	return !entry?:entry->value:0;
+	return entry!=0 ? entry->value : 0;
 }
 
 /**
- * @function set
- * @brief 设置
- * @param map
- * @param key
- * @param value
- * @return 0 success
- */
+* @function set
+* @brief 设置
+* @param map
+* @param key
+* @param value
+* @return 0 success
+*/
 int set(struct map * map, void * key, void * value)
 {
 	int first_hashcode = -1;
-	int hashcode = map->cal_hash_code(key) & (map->capacity - 1);
-	while((map->entries + hashcode)->key>0){
-		if(first_hashcode==-1){
+	int hashcode = (map->cal_hash_code(key)) & (map->capacity - 1);
+	while ((map->entries + hashcode)->flag == USING && map->cmp((map->entries + hashcode)->key, key)!=0){
+		if (first_hashcode == -1){
 			first_hashcode = hashcode;
-		} else if(first_hashcode == hashcode){
+		}
+		else if (first_hashcode == hashcode){
 			//第二次相等时说明遍历过一遍了
 			return -1; //没有空位
 		}
 		hashcode++;
-		if(hashcode>=map->capacity){
-			hashcode=-1;
+		if (hashcode >= map->capacity){
+			hashcode = 0;
 		}
 	}
 	(map->entries + hashcode)->key = key;
 	(map->entries + hashcode)->value = value;
-	return 0
+	(map->entries + hashcode)->flag = USING;
+	return 0;
 }
 
 
 /**
- * @function delete
- * @brief 删除
- * @param map
- * @param key
- * @return 0 success
- */
-int delete(struct map * map, void * key)
+* @function delete
+* @brief 删除
+* @param map
+* @param key
+* @return 0 success
+*/
+int del(struct map * map, void * key)
 {
 	struct entry * entry = find(map, key);
-	if(!entry){
+	if (entry == 0){
 		return -1;
 	}
-	entry->key = -1;
+	entry->key = 0;
 	entry->value = 0;
+	entry->flag = USED;
 	return 0;
-	
+
 }
--- kernel/include/common/map.h
+++ kernel/include/common/map.h
@@ -2,67 +2,71 @@
 #define _DIYOS_MAP_H
 
 /**
- * 计算key hash值
- */
-typedef int (* cal_hash_code_ptr) (void *);
+* 计算key hash值
+*/
+typedef int(*cal_hash_code_ptr) (void *);
 /**
- * 比较key的值
- */
-typedef int (* cmp_ptr)(void *, void *);
+* 比较key的值
+*/
+typedef int(*cmp_ptr)(void *, void *);
 
+#define USED	-1
+#define UNUSE	0
+#define USING	1
 /**
- * @struct entry
- * @brief map的entry
- * @field key 
- * @field value
- */
+* @struct entry
+* @brief map的entry
+* @field key
+* @field value
+*/
 struct entry {
 	void * key;
 	void * value;
+	int flag;   //-1 used 0 unuse 1 using
 };
 
 
 /**
- * @struct map
- * @brief hashmap
- * @param size;
- * @param capacity;
- * @param entry;
- * @param cal_hash_code;
- */
+* @struct map
+* @brief hashmap
+* @param size;
+* @param capacity;
+* @param entry;
+* @param cal_hash_code;
+*/
 struct map {
-		int size;							//entry数量
-		int capacity;						//总容量
-		struct entry * entries;				//连续内存
-		cal_hash_code_ptr cal_hash_code; 	//计算keyhash值的函数
-		cmp_ptr cmp;						//比较key的值
+	int size;							//entry数量
+	int capacity;						//总容量
+	struct entry * entries;				//连续内存
+	cal_hash_code_ptr cal_hash_code; 	//计算keyhash值的函数
+	cmp_ptr cmp;						//比较key的值
 };
 
 /**
- * @function get
- * @brief 根据key获取value
- * @param map
- * @param key
- * @return  value
- */
-extern void * get(sturct map * map, void * key);
+* @function get
+* @brief 根据key获取value
+* @param map
+* @param key
+* @return  value
+*/
+extern void * get(struct map * map, void * key);
 
 /**
- * @function set
- * @brief 设置
- * @param map
- * @param key
- * @param value
- * @return 0 success
- */
+* @function set
+* @brief 设置
+* @param map
+* @param key
+* @param value
+* @return 0 success
+*/
 extern int set(struct map * map, void * key, void * value);
 
 /**
- * @function delete
- * @brief 删除
- * @param map
- * @param key
- * @return 0 success
- */
-extern int delete(struct map * map, void * key);
+* @function delete
+* @brief 删除
+* @param map
+* @param key
+* @return 0 success
+*/
+extern int del(struct map * map, void * key);
 #endif
