3eb0dd50ac7bd2f7c625fab634d7b43cdeffa66f
Issac <aducode@126.com>
2015-08-03 19:23:29 +0800

    fix

--- kernel/include/fork.h
+++ kernel/include/fork.h
@@ -1,4 +1,10 @@
 #ifndef _DIYOS_FORK_H
 #define _DIYOS_FORK_H
+/**
+ * @function fork
+ * @brief fork出子进程
+ * 
+ * @return pid in parent process, 0 in child process
+ */
 extern int fork();
 #endif
--- kernel/include/getpid.h
+++ kernel/include/getpid.h
@@ -1,4 +1,9 @@
 #ifndef _DIYOS_GETPID_H
 #define _DIYOS_GETPID_H
+/**
+ * @function getpid
+ * @brief 获取当前进程的pid
+ * @return pid
+ */
 extern int getpid();
 #endif
