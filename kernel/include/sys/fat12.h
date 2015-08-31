3cb3532480525ef45dd84efaf19c33cb59ff94ae
Issac <aducode@126.com>
2015-08-31 19:38:28 +0800

    fix

--- kernel/include/sys/fat12.h
+++ kernel/include/sys/fat12.h
@@ -1,4 +1,32 @@
+#include "type.h"
 #ifndef _DIYOS_FAT12_H
 #define _DIYOS_FAT12_H
+/**
+ * @struct BPB
+ * @brief FAT12 Boot Parameter Block
+ * BPB位于引导扇区偏移11Byte处
+ *		之前有:
+ *			BS_jmpBOOT		3Byte
+ *			BS_OEMName		8Byte
+ */
+struct BPB {
+	u16 bytes_per_sec;		//每扇区字节数  
+	u8  sec_per_clus;		//每簇扇区数  
+	u16 rsvd_sec_cnt;		//Boot记录占用的扇区数  
+	u8  num_fats;			//FAT表个数  
+	u16 root_ent_cnt;		//根目录最大文件数  
+	u16 tot_sec16;			//扇区总数
+	u8  media;				//介质描述符
+	u16 fat_sz16;			//每FAT扇区数  
+	u16 sec_per_trk;		//每磁道扇区数
+	u16 num_heads;			//磁头数
+	u32 hidd_sec;			//隐藏扇区数
+	u32 tot_sec32;			//如果BPB_FATSz16为0，该值为FAT扇区数  
+};
 
+/**
+ * @function dump_bpb
+ * @brief dump出bpb的值
+ */
+extern void dump_bpb(struct BPB * bpb_ptr);
 #endif
--- kernel/task/fs/fat12.c
+++ kernel/task/fs/fat12.c
@@ -2,3 +2,22 @@
 /*                   fat12文件系统                    */
 /******************************************************/
 //主要供fs.c使用，用来将fat12格式的文件系统，适配到自己的文件系统
+#include "fat12.h"
+#include "assert.h"
+void dump_bpb(struct BPB *bpb_ptr)
+{
+	assert(bpb_ptr != 0);
+	printf("BPB:\n");
+	printf("\tBPB_BytsPerSec:%d\n", bpb_ptr->bytes_per_sec);
+	printf("\tBPB_SecPerClus:%d\n", bpb_ptr->sec_per_clus);
+	printf("\tBPB_RsvdSecCnt:%d\n", bpb_ptr->rsvd_sec_cnt);
+	printf("\tBPB_NumFATs:%d\n", bpb_ptr->num_fats);
+	printf("\tBPB_RootEntCnt:%d\n", bpb_ptr->root_ent_cnt);
+	printf("\tBPB_TotSec16:%d\n", bpb_ptr->tot_sec16);
+	printf("\tBPB_Media:%d\n", bpb_ptr->media);
+	printf("\tBPB_FATSz16:%d\n", bpb_ptr->num_fats);
+	printf("\tBPB_SecPerTrk:%d\n", bpb_ptr->sec_per_trk);
+	printf("\tBPB_NumHeads:%d\n", bpb_ptr->num_heads);
+	printf("\tBPB_HiddSec:%d\n", bpb_ptr->hidd_sec);
+	printf("\tBPB_TotSec32:%d\n", bpb_ptr->tot_sec32);
+}
